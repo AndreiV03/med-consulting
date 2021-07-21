744f5d8e1dcd99541e7d6ed5bb2d5ad7916be48c
AndreiV03 <andrei.voicu110@gmail.com>
2021-07-22 00:22:45 +0300

    Fix

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
 
 export default function NotFound() {
   return (
-    <div>
-      asdasd
+    <div className="not-found">
+      404 | Not Found
     </div>
   );
 }
