9593cd9bf2ee1f9ce394c6fca3d9fbb0787d9a10
Andrei Voicu <andrei.voicu110@gmail.com>
2021-09-16 19:46:04 +0300

    Update

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
-import { Dispatch, SetStateAction } from "react";
+import { useState, Dispatch, SetStateAction } from "react";
 import { BiSearch } from "react-icons/bi";
+import { motion, AnimatePresence, AnimateSharedLayout } from "framer-motion";
 
 import styles from "../styles/components/Menu.module.scss";
 
@@ -9,32 +10,94 @@ type Props = {
   setIsMenuOpen: Dispatch<SetStateAction<boolean>>;
 };
 
-const links: string[] = ["specializations", "services", "prices", "team", "contact"];
+const dropdownVariants = {
+  initial: {
+    height: 0,
+    transition: {
+      duration: 0.2,
+      ease: "easeIn",
+      stiffness: 0
+    }
+  }, 
+  animate: {
+    height: "auto",
+    transition: {
+      duration: 0.2,
+      ease: "easeIn",
+      stiffness: 0
+    }
+  },
+  exit: {
+    height: 0,
+    transition: {
+      duration: 0.2,
+      ease: "easeIn",
+      stiffness: 0
+    }
+  }
+};
 
 const Menu = ({ isMenuOpen, setIsMenuOpen }: Props): JSX.Element => {
+  const [isServicesDropdownOpen, setIsServicesDropdownOpen] = useState<boolean>(false);
+
+  const handleLinkClick = (): void => {
+    setIsServicesDropdownOpen(false);
+    setIsMenuOpen(false);
+  }
+
   return (
     <div className={!isMenuOpen ? styles.menu : `${styles.menu} ${styles.active}`} aria-hidden={!isMenuOpen}>
-      <div className={styles.links}>
-        {links.map(link => (
-          <Link href={`/${link}`} passHref key={link}>
-            <a className={styles.link} onClick={() => setIsMenuOpen(false)}>{link.charAt(0).toUpperCase() + link.slice(1)}</a>
+      <AnimateSharedLayout>
+        <div className={styles.links}>
+          <Link href="/specializations" passHref>
+            <a className={styles.link} onClick={handleLinkClick}>Specializations</a>
           </Link>
-        ))}
-      </div>
 
-      <div className={styles.buttons}>
-        <Link href="/search" passHref>
-          <a className={styles.search}><BiSearch /></a>
-        </Link>
+          <div className={styles.dropdown}>
+            <h3 className={styles.link} onClick={() => setIsServicesDropdownOpen(!isServicesDropdownOpen)}>Services</h3>
 
-        <Link href="/login" passHref>
-          <a className={styles.login}>Log in</a>
-        </Link>
+            <AnimatePresence>
+              {isServicesDropdownOpen && (
+                <motion.ul className={styles.container} aria-hidden={!isServicesDropdownOpen} layout initial="initial" animate="animate" exit="exit" variants={dropdownVariants}>
+                  <Link href="/services" passHref>
+                    <li className={styles.link} onClick={handleLinkClick}>Wellness and prevention</li>
+                  </Link>
 
-        <Link href="/register" passHref>
-          <a className={styles.register}>Sign up</a>
-        </Link>
-      </div>
+                  <Link href="/services" passHref>
+                    <li className={styles.link} onClick={handleLinkClick}>Everyday care</li>
+                  </Link>
+
+                  <Link href="/services" passHref>
+                    <li className={styles.link} onClick={handleLinkClick}>Chronic conditions</li>
+                  </Link>
+
+                  <Link href="/services" passHref>
+                    <li className={styles.link} onClick={handleLinkClick}>Mental health</li>
+                  </Link>
+                </motion.ul>
+              )}
+            </AnimatePresence>
+          </div>
+
+          <Link href="/prices" passHref>
+            <motion.a className={styles.link} layout onClick={handleLinkClick}>Prices</motion.a>
+          </Link>
+        </div>
+
+        <motion.div className={styles.buttons} layout>
+          <Link href="/search" passHref>
+            <a className={styles.search}><BiSearch /></a>
+          </Link>
+
+          <Link href="/login" passHref>
+            <a className={styles.login}>Log in</a>
+          </Link>
+
+          <Link href="/register" passHref>
+            <a className={styles.register}>Sign up</a>
+          </Link>
+        </motion.div>
+      </AnimateSharedLayout>
     </div>
   );
 }
