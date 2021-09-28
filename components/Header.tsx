e5b3420416b7623e5b8309bcead1938fdd88a771
Andrei Voicu <andrei.voicu110@gmail.com>
2021-09-28 20:35:21 +0300

    Update

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,55 +14,36 @@ type Props = {
 
 const Header = ({ isMenuOpen, setIsMenuOpen }: Props): JSX.Element => {
   const { scrollY } = useScroll();
-  useEffect(() => {}, [scrollY]);
 
-  const handleHeaderClasses = (): string => `${styles.header} ${isMenuOpen ? styles.active : ""} ${scrollY > 66 ? styles.fixed : ""} ${scrollY > 100 ? styles.scrolled : ""}`;
+  const handleHeaderClasses = (): string => `${styles.header} ${isMenuOpen && styles.menu_active} ${scrollY > 66 && styles.fixed} ${scrollY > 100 && styles.scrolled}`;
 
   useEffect(() => {
     const bodyElement: HTMLElement = document.body;
 
-    if (isMenuOpen)bodyElement.style.overflow = "hidden";
+    if (isMenuOpen) bodyElement.style.overflow = "hidden";
     else bodyElement.style.overflow = "visible";
   }, [isMenuOpen]);
 
   return (
     <header className={handleHeaderClasses()}>
-      <div className={!isMenuOpen ? styles.background : `${styles.background} ${styles.active}`} />
-
       <Link href="/" passHref>
-        <div className={!isMenuOpen ? styles.logo : `${styles.logo} ${styles.active}`} onClick={() => setIsMenuOpen(false)}><Logo /></div>
+        <div className={!isMenuOpen ? styles.logo : `${styles.logo} ${styles.menu_active}`} onClick={() => setIsMenuOpen(false)}><Logo /></div>
       </Link>
 
       <div className={styles.container}>
-        <div className={!isMenuOpen ? styles.links : `${styles.links} ${styles.active}`}>
-          <Link href="/specializations" passHref>
-            <a className={styles.link}>Specializations</a>
-          </Link>
-
-          <Link href="/services" passHref>
-            <a className={styles.link}>Services</a>
-          </Link>
-
-          <Link href="/prices" passHref>
-            <a className={styles.link}>Prices</a>
-          </Link>
+        <div className={!isMenuOpen ? styles.links : `${styles.links} ${styles.menu_active}`}>
+          <Link href="/specializations" passHref><a className={styles.link}>Specializations</a></Link>
+          <Link href="/services" passHref><a className={styles.link}>Services</a></Link>
+          <Link href="/prices" passHref><a className={styles.link}>Prices</a></Link>
         </div>
 
-        <div className={!isMenuOpen ? styles.links : `${styles.links} ${styles.active}`}>
-          <Link href="/login" passHref>
-            <a>Log in</a>
-          </Link>
-
-          <Link href="/register" passHref>
-            <a className={styles.register}>Sign up</a>
-          </Link>
-
-          <Link href="/search" passHref>
-            <a className={styles.search}><BiSearch /></a>
-          </Link>
+        <div className={!isMenuOpen ? styles.links : `${styles.links} ${styles.menu_active}`}>
+          <Link href="/login" passHref><a>Log in</a></Link>
+          <Link href="/register" passHref><a className={styles.register}>Sign up</a></Link>
+          <Link href="/search" passHref><a className={styles.search}><BiSearch /></a></Link>
         </div>
 
-        <div className={!isMenuOpen ? styles.button : `${styles.button} ${styles.active}`} onClick={() => setIsMenuOpen(!isMenuOpen)}>
+        <div className={!isMenuOpen ? styles.button : `${styles.button} ${styles.menu_active}`} onClick={() => setIsMenuOpen(!isMenuOpen)}>
           <MenuButton isMenuOpen={isMenuOpen} />
         </div>
       </div>
