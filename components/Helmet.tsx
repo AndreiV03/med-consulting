2c660f6ea9c730c46c5950a302b9955e5156240b
Andrei Voicu <andrei.voicu110@gmail.com>
2021-10-17 18:51:42 +0300

    Update

--- components/Helmet.tsx
+++ components/Helmet.tsx
@@ -15,12 +15,12 @@ const HelmetComponent: React.FC = () => {
 
         <meta name="description" content="" />
         <meta name="keywords" content="" />
-        <meta name="theme-color" content="#ffffff" />
+        <meta name="theme-color" content="#6633cc" />
         <meta name="canonical" content="" />
         <meta name="robots" content="noindex, nofollow" />
         <meta name="googlebot" content="noindex, nofollow" />
 
-        <link rel="manifest" href="manifest.json" />
+        <link rel="manifest" href="site.webmanifest" />
         <link rel="icon" href="favicon.ico" />
 
         <body className={isMenuOpen ? styles.menu_active : ""} />
