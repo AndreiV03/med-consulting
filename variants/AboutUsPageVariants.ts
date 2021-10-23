0fedf96a8886fc719eecba8da0f84921a659fb41
Andrei Voicu <andrei.voicu110@gmail.com>
2021-10-23 16:45:07 +0300

    Fix

--- variants/AboutUsPageVariants.ts
+++ variants/AboutUsPageVariants.ts
@@ -1,13 +1,13 @@
 export const heroSectionVariants = {
   initial: {
-    y: 50,
+    y: 25,
     opacity: 0
   },
   animate: {
     y: 0,
     opacity: 1,
     transition: {
-      duration: 0.5,
+      duration: 0.3,
       ease: "easeInOut",
       delay: 0.2,
       stiffness: 0
